7ddeca65ec3bbff5edd18253d88d08aa8bf67997
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2018-12-18 17:28:04 +0000

    drm/i915/perf: add NOA wait for gen8+
    In the course of introducing Gen8+ perf support, the initial patch had
    a usleep much like Gen7 to wait for the NOA configuration to apply.
    After a few revision, through testing, removing the wait didn't seem
    to introduce any change.
    
    In a follow up commit, it became apparent that this is actually
    required. This time we could verify this by doing in active wait on
    the command streamer.
    
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Fixes: 19f81df2859eb10e92d68991cefa39f826dea013 ("drm/i915/perf: Add OA unit support for Gen 8+")

--- drivers/gpu/drm/i915/i915_perf.c
+++ drivers/gpu/drm/i915/i915_perf.c
@@ -1802,6 +1802,29 @@ static int gen8_enable_metric_set(struct i915_perf_stream *stream)
 
 	config_oa_regs(dev_priv, oa_config->mux_regs, oa_config->mux_regs_len);
 
+	/* It apparently takes a fairly long time for a new MUX
+	 * configuration to be be applied after these register writes.
+	 * This delay duration was derived empirically based on the
+	 * render_basic config but hopefully it covers the maximum
+	 * configuration latency.
+	 *
+	 * As a fallback, the checks in _append_oa_reports() to skip
+	 * invalid OA reports do also seem to work to discard reports
+	 * generated before this config has completed - albeit not
+	 * silently.
+	 *
+	 * Unfortunately this is essentially a magic number, since we
+	 * don't currently know of a reliable mechanism for predicting
+	 * how long the MUX config will take to apply and besides
+	 * seeing invalid reports we don't know of a reliable way to
+	 * explicitly check that the MUX config has landed.
+	 *
+	 * It's even possible we've miss characterized the underlying
+	 * problem - it just seems like the simplest explanation why
+	 * a delay at this location would mitigate any invalid reports.
+	 */
+	usleep_range(15000, 20000);
+
 	config_oa_regs(dev_priv, oa_config->b_counter_regs,
 		       oa_config->b_counter_regs_len);
 
